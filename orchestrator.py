012c566d9d01227cc110f7f42dd5a7fd65930d5a
Ezajac <Eczajac@live.com>
2016-12-02 19:44:23 -0500

    Adding Main to Orchestrator

--- orchestrator.py
+++ orchestrator.py
@@ -27,6 +27,7 @@ import stock_analysis_price_earnings_v_industry as industry
 import stock_analysis_shares_short as shares_short
 #import pprint
 
+
 def Dict_Aggregator(data_dict, test_dict):
     if test_dict['Result'] == True or test_dict['Result'] == False:
         data_dict[test_dict['TestRun']] = test_dict['Result']
@@ -48,53 +49,58 @@ def Evaluator(data_dict):
     else:
         data_dict['Overall'] = False
     return data_dict
-    
+
+
 #Mongo DB Insert -> mods.Insert_Data('DB Name', 'Collection_Name', JSON Variable)
 #Mongo DB Name -> export variable
 export = 'test'
 stocks = ['AAPL', 'NVDA']
-for stock in stocks:
-    data = collections.OrderedDict()
-    data['DateRun'] = mods.Today()
-    data['Stock'] = stock
-    stock = stock.lower()
-    if mods.get_balance_sheet(stock, 'Common Stocks') != None:
-        #Revenue Test        
-        revenue_result = revenue.Get_Revenue(stock)
-        Dict_Aggregator(data, revenue_result)      
-        #4 Key Result Tests        
-        key_result = key.Get_Key_Indicators(stock)
-        for item in key_result:
-            Dict_Aggregator(data, item)
-        #Positive Return on Equity        
-        roe_result = roe.Get_ROE(stock)
-        Dict_Aggregator(data, roe_result)
-        #Analyst Opinion
-        opinion_result = opinion.Get_Analyst_Opinion(stock)
-        Dict_Aggregator(data, opinion_result)
-        #Earnings Forecast        
-        forecast_result = forecast.Get_Earnings_Forecast(stock)
-        Dict_Aggregator(data, forecast_result)
-        #Earnings Per Share        
-        eps_result = eps.Get_EPS(stock)
-        Dict_Aggregator(data, eps_result)
-        #Earnings Surprise        
-        eps_surprise = surprise.Get_Earnings_Surprise(stock)
-        Dict_Aggregator(data, eps_surprise)        
-        #Growth Forecast
-        growth = growth_forecast.Get_Growth_Forecast(stock)
-        Dict_Aggregator(data, growth) 
-        #PEG Ratio        
-        peg_ratio = peg.Get_PEG_Ratio(stock)
-        Dict_Aggregator(data, peg_ratio)
-        #Earnings Versus Industry        
-        pevi = industry.Get_Earnings_V_Industry(stock)
-        Dict_Aggregator(data, pevi)
-        #Insider Trading        
-        short = shares_short.Get_Shares_Short(stock)
-        Dict_Aggregator(data, short)
-    mods.Insert_Data(export, 'Results', Evaluator(data))
 
+def main():
+    for stock in stocks:
+        data = collections.OrderedDict()
+        data['DateRun'] = mods.Today()
+        data['Stock'] = stock
+        stock = stock.lower()
+        if mods.get_balance_sheet(stock, 'Common Stocks') != None:
+            #Revenue Test        
+            revenue_result = revenue.Get_Revenue(stock)
+            Dict_Aggregator(data, revenue_result)      
+            #4 Key Result Tests        
+            key_result = key.Get_Key_Indicators(stock)
+            for item in key_result:
+                Dict_Aggregator(data, item)
+            #Positive Return on Equity        
+            roe_result = roe.Get_ROE(stock)
+            Dict_Aggregator(data, roe_result)
+            #Analyst Opinion
+            opinion_result = opinion.Get_Analyst_Opinion(stock)
+            Dict_Aggregator(data, opinion_result)
+            #Earnings Forecast        
+            forecast_result = forecast.Get_Earnings_Forecast(stock)
+            Dict_Aggregator(data, forecast_result)
+            #Earnings Per Share        
+            eps_result = eps.Get_EPS(stock)
+            Dict_Aggregator(data, eps_result)
+            #Earnings Surprise        
+            eps_surprise = surprise.Get_Earnings_Surprise(stock)
+            Dict_Aggregator(data, eps_surprise)        
+            #Growth Forecast
+            growth = growth_forecast.Get_Growth_Forecast(stock)
+            Dict_Aggregator(data, growth) 
+            #PEG Ratio        
+            peg_ratio = peg.Get_PEG_Ratio(stock)
+            Dict_Aggregator(data, peg_ratio)
+            #Earnings Versus Industry        
+            pevi = industry.Get_Earnings_V_Industry(stock)
+            Dict_Aggregator(data, pevi)
+            #Insider Trading        
+            short = shares_short.Get_Shares_Short(stock)
+            Dict_Aggregator(data, short)
+        mods.Insert_Data(export, 'Results', Evaluator(data))
+
+if __name__ == '__main__':
+    main()
 '''
 Test and Collections
 1. Revenue #Done Revenue
